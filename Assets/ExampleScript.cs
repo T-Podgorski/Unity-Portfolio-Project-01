29452bd6b3b6a426bc0e090b72294d649c72131d
t-podgorski <147391857+T-Podgorski@users.noreply.github.com>
2023-10-10 21:28:40 +0200

    Example commit to a merged branch

--- Assets/ExampleScript.cs
+++ Assets/ExampleScript.cs
@@ -7,7 +7,7 @@ public class ExampleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log( "this is a test" );
     }
 
     // Update is called once per frame
