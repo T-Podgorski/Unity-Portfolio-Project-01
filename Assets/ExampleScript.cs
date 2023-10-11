5817e1316a9a54491aa8a2dae976b9719cc2bd87
t-podgorski <147391857+T-Podgorski@users.noreply.github.com>
2023-10-11 12:50:06 +0200

    main changed but this branch is behind test

--- Assets/ExampleScript.cs
+++ Assets/ExampleScript.cs
@@ -7,8 +7,7 @@ public class ExampleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log( "this is a test" );
-        Debug.Log( "this is another test" );
+        Debug.Log( "this is yet another test" );
     }
 
     // Update is called once per frame
