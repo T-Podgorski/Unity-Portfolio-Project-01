6a18eb5df4ec6c63d896ee28f00745d1eb359d41
t-podgorski <147391857+T-Podgorski@users.noreply.github.com>
2023-10-11 12:31:39 +0200

    main modified by another feature test

--- Assets/ExampleScript.cs
+++ Assets/ExampleScript.cs
@@ -7,8 +7,7 @@ public class ExampleScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log( "this is a test" );
-        Debug.Log( "this is another test" );
+        Debug.Log( "this is a multi branch test" );
     }
 
     // Update is called once per frame
